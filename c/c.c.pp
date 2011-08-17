f1aee884a5b7b95f04a6eb8bea592eaec848ae68
Scott Frazer <scott.d.frazer@gmail.com>
2011-08-17 12:51:13 -0400

    c.c tests verified

--- c/c.c.pp
+++ c/c.c.pp
@@ -14,7 +14,7 @@ int main()
 {
   printf("%d\n", 1000+7000);
   printf("%d\n", 1+2+7+4       );
-  printf("%d\n", foo66+33foo7+8);
+  printf("%d\n", foo 66 + 33 foo 7+8);
   printf("%d\n", 4+1+7-1     );
   return 0;
 }
