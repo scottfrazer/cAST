0859aaf4d721ed2de049e6ea6d2166ac3daa98cf
Scott Frazer <scott.d.frazer@gmail.com>
2011-11-09 23:38:01 -0500

    new test cases

--- tests/cases/c.c
+++ tests/cases/c.c
@@ -2,7 +2,7 @@
 #define qux qux0
 #define foo(x, y) x+y
 //          s  s  |c|
-#define bar foo foo(66,33) foo foo
+#define bar foo - foo(66,33) - foo - foo
 //          |---------c----------|
 #define baz foo(4,5)
 //              c c
@@ -12,6 +12,7 @@
 
 int main()
 {
+  int foo = 2;
   printf("%d\n", 1000+7000);
   printf("%d\n", var(1,2,qux,4));
   printf("%d\n", bar(7,8));
