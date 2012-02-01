f327caaffe0e30255f1db7d35f28fc26da294b0b
Scott Frazer <scott.d.frazer@gmail.com>
2012-02-01 08:30:24 -0500

    fixing unit tests

--- test/cases/reset.sh
+++ test/cases/reset.sh
@@ -1,5 +1,4 @@
 for file in *
 do
-  #rm $file/preprocessed $file/pptok $file/ppparse $file/ppast $file/ctok $file/cparse $file/cast 2>/dev/null
-  rm $file/pptok $file/ctok 2>/dev/null
+  rm $file/preprocessed $file/pptok $file/ppparse $file/ppast $file/ctok $file/cparse $file/cast 2>/dev/null
 done
